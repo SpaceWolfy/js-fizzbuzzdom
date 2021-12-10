8e608ba5905440bb24546c43cd8c01a0a9651bf4
SpaceWolfy <s_scozzari@libero.it>
2021-12-10 14:19:26 +0100

    First Milestone completed, added if in 1-100 cycle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 //Consegna: scrivi un programma che stampi in console i numeri da 1 a 100
 
 //Setup del codice di partenza:
-for (let i = 1; i <= 100; i++) {
+/* for (let i = 1; i <= 100; i++) {
     console.log(i)
+} */
+
+//Prima milestone:
+for (let i = 1; i <= 100; i++) 
+{
+    if(i % 3 === 0 && i % 5 === 0) 
+    {
+        console.log('FizzBuzz')
+    } else if(i % 3 === 0 ) 
+    {
+        console.log('Fizz')
+    } else if(i % 5 === 0) 
+    {
+        console.log('Buzz')
+    } else 
+    {
+        console.log(i)
+    }  
 }
